d0ff75e04032089c3548af199fb4e349bb0f103b
Bright Abety <bright@Brights-MacBook-Pro.local>
2022-10-23 22:46:14 -0400

    first draft complete

--- assets/jS/script.js
+++ assets/jS/script.js
@@ -1,29 +1,98 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
-    console.log('Started')
     startButton.classList.add('hide')
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion()
 }
 
-function selectAnswer() {
-
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classLisst.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
 }
 
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer() {
+    const selectedButton = e.target
+    const correct = selectButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    nextButton.classList.remove("hide")
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
 const questions = [
     {
-        question: 'How many bones are in the human body?', ''
+        question: "How many bones are in the human body?"
         answers: [
             { text: '206', correct: true },
             { text: '300', correct: false }
+        ]
+    },
+    {
+        question: 'What is the biggest planet in our solar system?'
+        answers: [
+            { text: 'Jupiter', correct: true },
+            { text: 'Earth', correct: false }
+        ]
+    },
+    {
+        question: "How many teeth  does an adult human have"
+        answers: [
+            { text: '32', correct: true },
+            { text: '69', correct: false }
+        ]
+    },
+    {
+        question: "Which is the main gas that makes up the Earth's atmosphete?"
+        answers: [
+            { text: 'Nitrogen', correct: true },
+            { text: 'Oxygen', correct: false }
         ]
     }
 ]
