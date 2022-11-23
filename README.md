06b8ae8b4d80e1b31b291f5f45a1cf4e9aa1a396
Bright Abety <bright@Brights-MacBook-Pro.local>
2022-11-23 13:26:39 -0500

    changes for regrading

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# 4 Web APIs Challenge: Code Quiz
+# 4 Web APIs: Code Quiz
+
+## Description
 This week's assignment is coding a quiz app online using JavaScript. In this week's assignment, I will be building a timed coding quiz with multiple-choice questions. This app is built to run in the browser with HTML and CSS that is powered by JavaScript.
+This assignment is by far the most challenging work I had to do since I started coding but Im gladly looking forward to what I create.
+
+## Links:
+Website: https://kagebright.github.io/brightquizcode/
+GitHub: https://github.com/kagebright/brightquizcode
+
+## Preview
--- assets/jS/script.js
+++ assets/jS/script.js
@@ -1,163 +1,160 @@
-//defining all variables
-const startElement = document.getElementById("start")
-const nextButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
-const countdownElement = document.getElementById("countdown");
-const scoreElement = document.getElementById("score");
-const highscoreElement = document.getElementById("highscore");
-const initialsEl = getElementById("initials");
+//defining all the variables for the quiz
+let countdownElement = document.getElementById("countdown");
+let startQuizEl = document.getElementById("startQuiz");
+let questionElement = document.getElementById("questions");
+let answerCheckEl = document.getElementById("answerCheck");
+let scoreEl = document.getElementById("score");
+let highscoreEl = document.getElementById("highscore");
+let initialsEl = document.getElementById("initials");
+let submitEl = document.getElementById("submit");
 let timeLeft = 60;
 let quizScore = 100;
 let index = 1;
-let games = 1;
+let games = [];
 
 //Questions for the quiz
-const questions = [
-    {   question: "How many bones are in the human body?",
-        options: {
-            1: "206",
-            2: "300",
-            3: "350",
-        },
-        correctAnswer: "206"
+let questionList = [
+{   question: "How many bones are in the human body?",
+    options: {
+        1: "206",
+        2: "300",
+        3: "350",
     },
-    {   question: "What is the biggest planet in our solar system?",
-        options: {
-            1: "Jupiter",
-            2: "Earth",
-            3: "Pluto",
-        },
-        correctAnswer: "Jupiter"    
+    correctAnswer: "206"
+},
+{   question: "What is the biggest planet in our solar system?",
+    options: {
+        1: "Jupiter",
+        2: "Earth",
+        3: "Pluto",
     },
-    {   question: "How many teeth  does an adult human have?",
-        options: {
-            1: "32",
-            2: "69",
-            3: "20",
-        },
-        correctAnswer: "32"
+    correctAnswer: "Jupiter"    
+},
+{   question: "How many teeth  does an adult human have?",
+    options: {
+        1: "32",
+        2: "69",
+        3: "20",
     },
-    {   question: "Which is the main gas that makes up the Earth's atmosphete?",
-        options: {
-            1: "Nitrogen",
-            2: "Oxygen",
-            3: "Carbon dioxide",
-         },
-         correctAnswer: "Nitrogen"
-    },
-    {   question: "Which is the main gas that makes up the Earth's atmosphete?",
-        options: {
+    correctAnswer: "32"
+},
+{   question: "Which is the main gas that makes up the Earth's atmosphete?",
+    options: {
         1: "Nitrogen",
         2: "Oxygen",
         3: "Carbon dioxide",
-        },
-        correctAnswer: "Nitrogen"
-    }
+     },
+     correctAnswer: "Nitrogen"
+},
+{   question: "Which is the main gas that makes up the Earth's atmosphete?",
+    options: {
+    1: "Nitrogen",
+    2: "Oxygen",
+    3: "Carbon dioxide",
+    },
+    correctAnswer: "Nitrogen"
+}
 ];
 
-//code for the countdown when the start button is clicked
-startButton.addEventListener("click", function () {
-    countdownElement();
-    renderQuestions(questions[0]);
+
+//when the start button is clicked, the countdown will begin and the questions will be asked
+startQuizEl.addEventListener("click", function () {
+  countdown();
+  renderQuestions(questionList[0]);
 });
 
-//continuation of the quiz after each question
+//This is so the game continues to ask questions as you complete each question until you get to the last one
 questionElement.addEventListener("click", function(event) {
-    if (index == 5) {
-        alert("Thank you for participating! Game Over! Please submit your and try again");
-    } else if (event.target.textContent != questions[index - 1].correctAnswer) {
-        timeLeft -=10;
+  if (index == 5) {
+      alert("Thank you for Testing your JavaScript Knowledge, the game is over. Please submit your score and refresh the browser to try again");
+  } else if (event.target.textContent != questionList[index - 1].correctAnswer) {
+    timeLeft -= 10;
+  } else {
+    index++; 
+    console.log(timeLeft);
+    renderQuestions(questionList[index - 1]);
+  }
+});
+//The User starts with 200 points and for each question he gets wrong, 20 is deducted
+questionElement.addEventListener("click", function(event) {
+  if (event.target.textContent === questionList[index - 1].correctAnswer) {
+      quizScore += 20;
+      scoreEl.textContent = "Your score = " + quizScore
+  }else {
+      quizScore -= 20;
+      scoreEl.textContent = "Your score = " + quizScore
+      setTimeout(questionList[index - 1]);
+  }
+});
+ // Timer that counts down from 80 seconds
+ function countdown() {
+  let timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      countdownElement.textContent = timeLeft;
+      timeLeft--;
     } else {
-        index++;
-        console.log(timeLeft);
-        renderQuestions(questions[index - 1]);
+      alert("Time is Up");
+      clearInterval(timeInterval);
     }
-});
+  }, 1000);
+}
+// This functions is used to start the quiz and so the viewer could see the questions on display
+function renderQuestions(validQuestion) {
+  questionElement.innerHTML = "";
 
-//100 points system, 20 points per question
-questionElement.addEventListener("click",function(event) {
-   if (event.target.textConttent === questions[index -1].correctAnswer) {
-    quizScore += 20;
-    scoreElement.textContent = "Your score = " + quizScore;
-   } else {
-    quizScore -= 20;
-    scoreElement.textContent = "Your score = " + quizScore;
-    setTimeout(questions[index - 1]);
-} 
-});
+  // creates the questions and options and then appends them to the screen. Saw a youtube video on how to do this
+  let questionAsked = document.createElement("p");
+  let optionsList = document.createElement("ol");
+  let option1 = document.createElement("li");
+  let option2 = document.createElement("li");
+  let option3 = document.createElement("li");
 
-//60 seconds countdown
-function countdown() {
-    let timeInterval = setInterval(function() {
-        if (timeLeft >1) {
-            countdownElement.textContent = timeLeft;
-            timeLeft--;
-        } else {
-            alert("Time's Up!");
-            clearInterval(timeInterval);
-        }
-    }, 1000);
+  questionAsked.textContent = validQuestion.question;
+  option1.textContent = validQuestion.options[1];
+  option2.textContent = validQuestion.options[2];
+  option3.textContent = validQuestion.options[3];
+
+
+  questionElement.append(questionAsked);
+  questionElement.append(optionsList);
+  optionsList.append(option1);
+  optionsList.append(option2);
+  optionsList.append(option3);
 }
 
-function renderQuestions(validQuestion) {
-    questionElement.innerHTML = "";
+// Setting up local storage to add highschore and initials to it.
   
-    // creates the questions and options and then appends them to the screen. Saw a youtube video on how to do this
-    let questionAsked = document.createElement("p");
-    let optionsList = document.createElement("ol");
-    let option1 = document.createElement("li");
-    let option2 = document.createElement("li");
-    let option3 = document.createElement("li");
-    let option4 = document.createElement("li");
-  
-    questionAsked.textContent = validQuestion.question;
-    option1.textContent = validQuestion.options[1];
-    option2.textContent = validQuestion.options[2];
-    option3.textContent = validQuestion.options[3];
-    option4.textContent = validQuestion.options[4];
-  
-    questionElement.append(questionAsked);
-    questionElement.append(optionsList);
-    optionsList.append(option1);
-    optionsList.append(option2);
-    optionsList.append(option3);
-    optionsList.append(option4);
+submitEl.addEventListener("click", function (event) {
+  event.preventDefault();
+  let game = {
+    initials: initialsEl.value,
+    score: quizScore,
+  };
+  games.push(game);
+  privateData();
+  renderTheGame();
+});
+
+function renderTheGame() {
+  for (let i = 0; i < games.length; i++) {
+    console.log(games[i]);
+    let highScores = document.createElement("li");
+    highScores.textContent = games[i].initials + games[i].score;
+    highscoreEl.append(highScores);
   }
-  
-  // Setting up local storage for highscores and storage
-  submitEl.addEventListener("click", function (event) {
-    event.preventDefault();
-    let game = {
-      initials: initialsEl.value,
-      score: quizScore,
-    };
-    games.push(game);
-    privateData();
-    renderTheGame();
-  });
-  
-  function renderTheGame() {
-    for (let i = 0; i < games.length; i++) {
-      console.log(games[i]);
-      let highScores = document.createElement("li");
-      highScores.textContent = games[i].initials + games[i].score;
-      highscoreEl.append(highScores);
-    }
-  
-    console.log(games.length);
+
+  console.log(games.length);
+}
+
+function privateData() {
+  console.log(games);
+  localStorage.setItem("games", JSON.stringify(games));
+}
+function storeData() {
+  let privateData = JSON.parse (localStorage.getItem("games"));
+  if (privateData !== null) {
+    games = privateData;
   }
-  
-  function privateData() {
-    console.log(games);
-    localStorage.setItem("games", JSON.stringify(games));
-  }
-  function storeData() {
-    let privateData = JSON.parse (localStorage.getItem("games"));
-    if (privateData !== null) {
-      games = privateData;
-    }
-  }
-  
-  storeData();
+}
+
+storeData();
